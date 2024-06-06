43c4d9e67ab8e15cf07c156565bbc614664e1b21
TNHS <sontnh@twinger.vn>
2024-06-06 18:22:04 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import { themeOptions } from './configs/mui/theme'
 import './index.css'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Button>This button has disabled ripples.</Button>
+      <Typography color="text.primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nulla mollitia omnis adipisci magnam vel doloribus quas expedita fugit, consequuntur ipsa quae ex corporis voluptas laborum exercitationem ea voluptatum! Quidem.</Typography>
     </ThemeProvider>
   </React.StrictMode>,
 )
